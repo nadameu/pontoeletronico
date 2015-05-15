c3293d05a252cfd6d0f2b763c7d024f4761f03ef
Paulo <paulo@nadameu.com.br>
2015-05-15 14:20:11 -0300

    Atualização para lidar com feriados e finais de semana.

--- pontoeletronico.user.js
+++ pontoeletronico.user.js
@@ -4,13 +4,60 @@
 // @description Relatório de ponto eletrônico
 // @require     https://code.jquery.com/jquery-2.1.1.min.js
 // @include     http://apl.jfpr.gov.br/pe/App_View/relatorio_1.aspx
-// @version     4
+// @version     5
 // @grant       none
 // ==/UserScript==
 var MINUTOS_DE_TOLERANCIA = 15;
+var FERIADOS = {
+  '*': [
+    '01/01',
+    '02/01',
+    '03/01',
+    '04/01',
+    '05/01',
+    '06/01',
+    '02/02',
+    '21/04',
+    '01/05',
+    '11/08',
+    '07/09',
+    '12/10',
+    '28/10',
+    '01/11',
+    '02/11',
+    '20/11',
+    '08/12',
+    '09/12',
+    '20/12',
+    '21/12',
+    '22/12',
+    '23/12',
+    '24/12',
+    '25/12',
+    '26/12',
+    '27/12',
+    '28/12',
+    '29/12',
+    '30/12',
+    '31/12'
+  ],
+  '2015': [
+    '16/02',
+    '17/02',
+    '01/04',
+    '02/04',
+    '03/04',
+    '04/06'
+  ]
+};
 function analisar() {
   var jornada = obterJornada();
   var faltas = obterFaltas();
+  var diasUteis = obterDiasUteis();
+  var diasUteisTrabalhados = obterDiasUteisTrabalhados();
+  var diasNaoUteis = obterDiasNaoUteis();
+  var diasNaoUteisTrabalhados = 0;
+  var diasTrabalhados = 0;
   var registroAnterior = {
     timestamp: null,
     data: null,
@@ -18,9 +65,16 @@ function analisar() {
     celula: null
   },
   ultimaEntrada,
-  somaParcial = 0 - jornada.valueOf(),
-  somaTotal = 0 - (faltas * jornada);
-  var linhas = $('#ctl00_ContentPlaceHolder1_GridView1 tbody tr').has('td');
+  somaParcial = 0,
+  somaTotal = 0 - (diasUteis * jornada);
+  $('#ctl00_ContentPlaceHolder1_GridView1 tbody tr') .has('th') .each(function (indice, elemento) {
+    if ($(elemento) .has('#tituloColunaSaldo') .size() > 0) {
+      return ;
+    }
+    $(elemento) .append('<th id="tituloColunaSaldo">Saldo</th>');
+  }
+  );
+  var linhas = $('#ctl00_ContentPlaceHolder1_GridView1 tbody tr') .has('td');
   var numLinha = 0;
   var linha = linhas.get(numLinha);
   var registro = linhaParaRegistro(linha);
@@ -52,9 +106,17 @@ function analisar() {
     if (ultimaLinhaDoDia) {
       linha.style.borderBottom = '2px solid black';
       var minutos = milissegundosParaMinutos(somaParcial);
-      registro.celula.textContent = formatarMinutos(minutos);
-      registro.celula.style.color = (minutos < 0) ? 'red' : 'green';
-      if (Math.abs(minutos) >= MINUTOS_DE_TOLERANCIA) {
+      var saldo;
+      if (ehFeriado(registro)) {
+        saldo = minutos;
+      } else {
+        saldo = minutos - milissegundosParaMinutos(jornada.valueOf());
+        somaTotal += jornada.valueOf();
+        somaParcial -= jornada.valueOf();
+      }
+      registro.celula.textContent = formatarMinutos(saldo);
+      registro.celula.style.color = (saldo < 0) ? 'red' : 'green';
+      if (Math.abs(saldo) >= MINUTOS_DE_TOLERANCIA) {
         registro.celula.style.textDecoration = 'none';
         registro.celula.style.fontWeight = 'bold';
         somaTotal += somaParcial;
@@ -67,26 +129,64 @@ function analisar() {
         registro.celula.style.color = 'white';
         registro.tipo = 'S';
       }
-      somaParcial = 0 - jornada.valueOf();
+      somaParcial = 0;
+      diasTrabalhados++;
     }
     registroAnterior = registro;
     registro = proximoRegistro;
     linha = proximaLinha;
   } while (numLinha < linhas.size());
   var saldo = $('#ctl00_ContentPlaceHolder1_lblSalR');
-  saldo.html(formatarMinutos(milissegundosParaMinutos(somaTotal))).css('color', (somaTotal < 0) ? 'red' : 'green');
-  saldo.parent().html(saldo);
+  saldo.html(formatarMinutos(milissegundosParaMinutos(somaTotal))) .css('color', (somaTotal < 0) ? 'red' : 'green');
+  saldo.parent() .html(saldo);
   saldo.after('<br/><span style="font-size: 0.8em;"> (ignorando diferenças inferiores a ' + MINUTOS_DE_TOLERANCIA + ' minutos de tolerância).</span>');
+  definirDiasNaoUteisTrabalhados(diasTrabalhados - diasUteisTrabalhados);
 }
 function obterJornada() {
-  var texto = $('#ctl00_ContentPlaceHolder1_lblJornR').get(0).textContent;
+  var texto = $('#ctl00_ContentPlaceHolder1_lblJornR') .get(0) .textContent;
   var valor = textoParaData('01/01/2001 ' + texto) - textoParaData('01/01/2001 00:00:00');
   return valor;
 }
 function obterFaltas() {
-  var texto = $('#ctl00_ContentPlaceHolder1_lblFaltasR').get(0).textContent;
+  var elemento = $('#ctl00_ContentPlaceHolder1_lblFaltasR');
+  var texto = elemento.get(0) .textContent;
+  var valor = Number(texto);
+  if (valor > 0) elemento.css({
+    'color': 'red',
+    'font-weight': 'bold'
+  });
+  return valor;
+}
+function obterDiasUteis() {
+  var texto = $('#ctl00_ContentPlaceHolder1_lblDiaUR') .get(0) .textContent;
   var valor = Number(texto);
   return valor;
+}
+function obterDiasUteisTrabalhados() {
+  var texto = $('#ctl00_ContentPlaceHolder1_lblDUTR') .get(0) .textContent;
+  var valor = analisarValorTrabalhados(texto);
+  return valor;
+}
+function obterDiasNaoUteis() {
+  var texto = $('#ctl00_ContentPlaceHolder1_lblSDFPR') .get(0) .textContent;
+  var valor = Number(texto);
+  return valor;
+}
+function definirDiasNaoUteisTrabalhados(valor) {
+  var estilo = '';
+  if (valor > 0) {
+    estilo = 'color: green; font-weight: bold;';
+  }
+  $('#ctl00_ContentPlaceHolder1_lblSDFR') .html('(<span style="' + estilo + '">' + valor + '</span> trabalhados)');
+}
+function analisarValorTrabalhados(texto) {
+  var re = /^\((\d+) trabalhados\)$/;
+  var resultado = re.exec(texto);
+  if (resultado.length == 2) {
+    return Number(resultado[1]);
+  } else {
+    throw new Error('Texto "' + texto + '" não formatado conforme esperado.');
+  }
 }
 function linhaParaRegistro(linha) {
   var registro = {
@@ -102,13 +202,17 @@ function linhaParaRegistro(linha) {
   while (mes.length < 2) {
     mes = '0' + mes;
   }
-  registro.data = [ano, mes, dia].join('-');
+  registro.data = [
+    ano,
+    mes,
+    dia
+  ].join('-');
   registro.celula = $('.resultado', linha);
   if (registro.celula.size() == 1) {
     registro.celula = registro.celula.get(0);
   } else {
-    registro.celula = $('<td class="resultado"></td>').get(0);
-    $(linha).append(registro.celula);
+    registro.celula = $('<td class="resultado"></td>') .get(0);
+    $(linha) .append(registro.celula);
   }
   return registro;
 }
@@ -130,7 +234,7 @@ function formatarMinutos(minutos) {
   var sinal = minutos / minutosAbsoluto;
   var h = Math.floor(minutosAbsoluto / 60);
   var m = Math.floor(minutosAbsoluto % 60);
-  while (m.toString().length < 2) {
+  while (m.toString() .length < 2) {
     m = '0' + m;
   }
   return (sinal < 0 ? '-' : '') + [h,
@@ -139,7 +243,7 @@ function formatarMinutos(minutos) {
 var botao = criarBotao();
 anexarBotaoAoMenu(botao);
 function criarBotao() {
-  return $('<button></button>').html('Analisar').on('click', function (ev) {
+  return $('<button></button>') .html('Analisar') .on('click', function (ev) {
     ev.preventDefault();
     analisar();
   });
@@ -147,4 +251,22 @@ function criarBotao() {
 function anexarBotaoAoMenu(botao) {
   var menu = $('#ctl00_ctl02');
   menu.before(botao);
+}
+function ehFeriado(registro) {
+  if (registro.timestamp.getDay() == 0 || registro.timestamp.getDay() == 6) {
+    return true;
+  }
+  var [ano,
+  mes,
+  dia] = registro.data.split('-'),
+  data = [
+    dia,
+    mes
+  ].join('/');
+  if (FERIADOS['*'].indexOf(data) > - 1) {
+    return true;
+  } else if (FERIADOS[ano].indexOf(data) > - 1) {
+    return true;
+  }
+  return false;
 }
