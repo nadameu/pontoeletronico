e6c68be9aa4a29df5380cf516dd44398fca1130f
Paulo <paulo@nadameu.com.br>
2016-03-03 19:36:04 -0300

    Mais uma

--- pontoeletronico.user.js
+++ pontoeletronico.user.js
@@ -81,39 +81,66 @@ function analisarRegistros() {
   var paiTabela = elementoTabela.parentNode;
   paiTabela.removeChild(elementoTabela);
 
-  tabela.find('tbody tr:has(th):not(:has(#tituloColunaSaldo))').each((indice, elemento) => $(elemento).append('<th id="tituloColunaSaldo">Saldo</th>'));
+  tabela.find('tbody tr:has(th):not(:has(#tituloColunaSaldo))').each(function(indiceLinha, linha) {
+    linha.cells[3].textContent = 'Justificativa';
+    linha.deleteCell(4);
+    $(linha).append('<th id="tituloColunaSaldo">Saldo</th><th>Motivo</th>')
+  });
 
   var linhas = Array.prototype.slice.call(tabela.find('tbody tr:has(td)'));
   intervalo.analisarLinhas(linhas);
-
+/*
   var datas = Object.keys(intervalo);
   var indice = datas.length - 1;
   var contagem = 0;
   while (contagem <= PRESCRICAO && indice > -1) {
+    ++contagem;
     var dataAtual = datas[indice];
     var dia = intervalo[dataAtual];
     if (dia.compensacao || (dia.falta && (dia instanceof DiaUtil))) {
       contagem = 0;
+    } else if (dia.zerado) {
+      contagem = PRESCRICAO;
+      break;
     }
-    ++contagem;
     --indice;
   }
+  var dataAConsiderar = DateFactory.deslocarDias(dia.data, contagem - PRESCRICAO);
+  console.log(DateHelper.toLocaleDate(dataAConsiderar));
   
   for (var i = indice; i > -1; --i) {
-    intervalo[ datas[i] ].saldoConsiderado = 0;
+    var dia = intervalo[ datas[i] ];
+    dia.saldoConsiderado = 0;
+    if (contagem === PRESCRICAO) {
+      dia.motivo = 'Zerado';
+    } else {
+      dia.motivo = 'Prescrito';
+    }
   }
   
+  if (contagem > 0) {
+    intervalo.analisarCompensacoes(dataInicio, dataFim);
+  } else {
+    intervalo.analisarCompensacoes(dataAConsiderar, dataFim);
+  }
+  */
+  intervalo.analisarCompensacoes(dataInicio, dataFim);
   intervalo.inserirLinhasEm(tbody);
   
   var saldo = $('#ctl00_ContentPlaceHolder1_lblSalR');
   saldo.html(IntervalHelper.toMinutesString(intervalo.saldoConsiderado)).css('color', (intervalo.saldoConsiderado < 0) ? '#c33' : '#262');
   var tabelaSaldo = saldo.parents('table:first');
-  var aviso = $('<p>Ignorando diferenças inferiores a ' + MINUTOS_DE_TOLERANCIA + ' minutos de tolerância.</p>').css({textAlign: 'right', fontSize: '0.8em'});
+  var aviso = $('<p style="font-family: Arial; color: #696969;">Ignorando diferenças inferiores a ' + MINUTOS_DE_TOLERANCIA + ' minutos de tolerância.</p>').css({textAlign: 'right', fontSize: '0.8em'});
   tabelaSaldo.after(aviso);
-  if (dataInicio > intervalo[dataAtual].data) {
-    aviso.append('<br/><span style="color: #c33;">Para cálculo do saldo correto selecione como data de início:<br/>' + DateHelper.toDateExtenso(intervalo[dataAtual].data) + '.</span>');
-  } else {
-    aviso.append('<br/>Considerando apenas registros a partir de ' + DateHelper.toLocaleDate(intervalo[dataAtual].data) + ' (' + PRESCRICAO +' dias de prescrição).');
+  if (dataInicio.getTime() > intervalo.dataIdeal.getTime()) {
+    aviso.append('<br/><span style="color: #c33;">Para cálculo do saldo correto selecione como data de início:<br/>' + DateHelper.toDateExtenso(intervalo.dataIdeal) + '.</span>');
+  } else if (dataInicio.getTime() < intervalo.dataConsiderada.getTime()) {
+    aviso.append('<br/>Considerando apenas registros a partir de ' + DateHelper.toLocaleDate(intervalo.dataConsiderada.getTime()));
+    if (intervalo.dataConsiderada.getTime() !== intervalo.dataIdeal.getTime()) {
+      aviso.append(' (saldo zerado).');
+    } else {
+      aviso.append(' (' + PRESCRICAO +' dias de prescrição).');
+    }
   }
     
   definirDiasTrabalhados(intervalo.diasUteis, intervalo.diasUteisTrabalhados, intervalo.feriados, intervalo.feriadosTrabalhados);
@@ -124,7 +151,7 @@ function analisarRegistros() {
 
 function obterJornada() {
   var texto = $('#ctl00_ContentPlaceHolder1_lblJornR').get(0).textContent;
-  return DateFactory.hmsTexto(texto);
+  return DateFactory.hmsTexto(texto).getTime();
 }
 
 function obterDataInicio() {
@@ -203,7 +230,7 @@ var DateHelper = (function() {
 
 var IntervalHelper = {
   toMinutes: function(interval) {
-    return Math.round(interval / 60 / 1000);
+    return (interval / 60 / 1000) | 0;
   },
   toMinutesString: function(interval) {
     var minutos = IntervalHelper.toMinutes(interval);
@@ -221,23 +248,26 @@ var IntervalHelper = {
 function Dia(data) {
   this.data = data;
   this.registros = [];
+  this.saldoConsiderado = this.saldo = 0 - this.jornadaPadrao;
 }
 Dia.prototype = {
   compensacao: false,
   data: null,
   falta: true,
   jornadaPadrao: 0,
+  motivo: '',
   registros: null,
   saldo: 0,
   saldoConsiderado: 0,
   trabalhado: 0,
   ultimoRegistro: null,
+  zerado: false,
   inserirLinhasEm: function(tbody) {
     this.getLinhas().forEach(function(linha, indiceLinha) {
       tbody.appendChild(linha);
     });
     if (this.registros.length !== 0) {
-      this.ultimoRegistro.formatarUltimoRegistro(this.saldo, this.saldoConsiderado);
+      this.ultimoRegistro.formatarUltimoRegistro(this.saldo, this.saldoConsiderado, this.motivo);
     }
   },
   inserirRegistro: function(registro) {
@@ -255,15 +285,21 @@ Dia.prototype = {
         registro.tipo = 'S';
       }
       this.trabalhado += registro.dataHora.getTime() - this.ultimoRegistro.dataHora.getTime();
-      this.saldo = this.trabalhado - this.jornadaPadrao.getTime();
+      this.saldo = this.trabalhado - this.jornadaPadrao;
       if (Math.abs(this.saldo) < MINUTOS_DE_TOLERANCIA * 60 * 1000) {
         this.saldoConsiderado = 0;
+        this.motivo = '<' + MINUTOS_DE_TOLERANCIA + 'min';
       } else {
         this.saldoConsiderado = this.saldo;
       }
     }
+    if (registro.dataHora.getTime() !== registro.alteracao.dataHora.getTime()) {
+      registro.destacarRegistroAlterado();
+    }
     if (registro.justificativa === 'Compensação por serviço extraordinário') {
       this.compensacao = true;
+    } else if (/zerado/i.exec(registro.justificativa)) {
+      this.zerado = true;
     }
     this.ultimoRegistro = registro;
     this.falta = false;
@@ -287,7 +323,7 @@ Feriado.prototype.getLinhas = function() {
   if (this.falta) {
     return [];
   } else {
-    return Array.map(this.registros, (registro) => registro.linha);
+    return Array.map(this.registros, (registro) => registro.getLinha());
   }
 };
 
@@ -300,7 +336,7 @@ DiaUtil.prototype.getLinhas = function() {
   if (this.falta) {
     this.ultimoRegistro = this.registros[this.registros.length++] = new Falta(this.data);
   }
-  return Array.map(this.registros, (registro) => registro.linha);
+  return Array.map(this.registros, (registro) => registro.getLinha());
 };
 
 DiaUtil.definirJornadaPadrao = function(jornadaPadrao) {
@@ -309,6 +345,8 @@ DiaUtil.definirJornadaPadrao = function(jornadaPadrao) {
 
 function Intervalo(inicio, fim) {
   Object.defineProperties(this, {
+    dataConsiderada: { value: null, writable: true },
+    dataIdeal: { value: null, writable: true },
     diasUteis: { value: 0, writable: true },
     diasUteisTrabalhados: { value: 0, writable: true },
     feriados: { value: 0, writable: true },
@@ -327,11 +365,85 @@ function Intervalo(inicio, fim) {
 }
 Intervalo.prototype = Object.create(null, {
   constructor: { value: Intervalo },
+  dataConsiderada: { value: null },
+  dataIdeal: { value: null },
   diasUteis: { value: 0 },
   diasUteisTrabalhados: { value: 0 },
   feriados: { value: 0 },
   feriadosTrabalhados: { value: 0 },
   saldoConsiderado: { value: 0 },
+  analisarCompensacoes: {
+    value: function(inicio, fim) {
+      var datas = Object.keys(this);
+      var contagemPrescricao = PRESCRICAO;
+      var dataIdeal, dataConsiderada;
+      for (var indice = datas.length - 1; contagemPrescricao > -1 && indice > -1; --indice) {
+        var dia = this[ datas[indice] ];
+        if (dia.compensacao || (dia.falta && (dia instanceof DiaUtil))) {
+          contagemPrescricao = PRESCRICAO;
+        } else if (dia.zerado) {
+          contagemPrescricao = -2;
+          dataIdeal = dia.data;
+          dataConsiderada = DateFactory.diaSeguinte(dia.data);
+          break;
+        } else {
+          --contagemPrescricao;
+        }
+      }
+      if (contagemPrescricao === -1) {
+        dataIdeal = dataConsiderada = dia.data;
+      } else if (indice === -1) {
+        ++contagemPrescricao;
+        dataConsiderada = dia.data;
+        dataIdeal = DateFactory.deslocarDias(dataConsiderada, -contagemPrescricao);
+      }
+      this.dataIdeal = dataIdeal;
+      this.dataConsiderada = dataConsiderada;
+      return;
+      
+      var indice, indiceCompensacao;
+      indice = indiceCompensacao = datas.indexOf(DateHelper.toISODate(inicio));
+      for (var len = datas.length; indice < len; ++indice) {
+        var dataAtual = datas[indice];
+        var dia = this[dataAtual];
+//        if (dia.compensacao || (dia.falta && (dia instanceof DiaUtil))) {
+        if (dia.saldoConsiderado < 0) {
+          console.log('data a compensar:', DateHelper.toLocaleDate(dia.data));
+          console.log('dias entre última compensação e hoje:', indice - indiceCompensacao);
+          var saldo = dia.saldoConsiderado;
+//          for (; saldo < 0 && indiceCompensacao < indice; ++indiceCompensacao) {
+          for (; saldo < 0 && indiceCompensacao < len; ++indiceCompensacao) {
+            var dataCompensacao = datas[indiceCompensacao];
+            var diaCompensacao = this[dataCompensacao];
+            if (diaCompensacao.saldoConsiderado > 0) {
+              var diminuir = Math.min(diaCompensacao.saldoConsiderado, -saldo);
+
+              saldo += diminuir;
+              dia.motivo = 'Parcialmente compensado';
+
+              diaCompensacao.saldoConsiderado -= diminuir;
+              if (diaCompensacao.saldoConsiderado == 0) {
+                diaCompensacao.motivo = 'Compensado';
+                console.log('Dia', DateHelper.toLocaleDate(diaCompensacao.data),'colaborou.');
+              } else {
+                diaCompensacao.motivo = 'Parcialmente compensado';
+                console.log('Dia', DateHelper.toLocaleDate(diaCompensacao.data),'compensou integralmente.');
+                break;
+              }
+            }
+          }
+          dia.saldoConsiderado = saldo;
+          if (saldo === 0) {
+            dia.motivo = 'Compensado';
+          }
+        }
+      }
+      for (; indiceCompensacao < len; ++indiceCompensacao) {
+        console.log('implementar prescrição dos remanescentes');
+        
+      }
+    }
+  },
   analisarLinhas: {
     value: function(linhas) {
       var datas = Object.keys(this);
@@ -359,6 +471,9 @@ Intervalo.prototype = Object.create(null, {
         var dia = this[data];
         this.saldoConsiderado += dia.saldoConsiderado;
         dia.inserirLinhasEm(tbody);
+        if (dia.zerado) {
+          this.saldoConsiderado = 0;
+        }
       }
     }
   },
@@ -394,7 +509,10 @@ Registro.prototype = {
   destacarRegistroAlterado: function() {
      this.linha.cells[1].classList.add('alterado');
   },
-  formatarUltimoRegistro: function(saldo, saldoConsiderado) {
+  getLinha: function() {
+    return this.linha;
+  },
+  formatarUltimoRegistro: function(saldo, saldoConsiderado, motivo) {
     this.linha.className = 'ultima';
     var celula;
     celula = this.linha.insertCell();
@@ -410,6 +528,7 @@ Registro.prototype = {
       classes.push('erro');
     }
     celula.className = classes.join(' ');
+    this.linha.insertCell().textContent = motivo;
   }
 };
 Registro.prototype.constructor = Registro;
@@ -424,6 +543,9 @@ Registro.fromLinha = function(linha) {
   
   var textoData = DateHelper.toISODate(dataHora);
   
+  linha.cells[3].textContent = justificativa;
+  linha.deleteCell(4);
+  
   var registro = new Registro();
   registro.linha = linha;
   registro.dataHora = dataHora;
@@ -437,7 +559,7 @@ Registro.fromLinha = function(linha) {
 
 function Falta(data) {
   var celulaVazia = '<td><br/></td>';
-  this.linha = $('<tr class="ultima" style="font-family: Arial; font-size: 8pt;"><td>' + DateHelper.toLocaleDate(data) + '</td>' + celulaVazia + '<td class="erro">Falta</td>' + celulaVazia.repeat(3) + '</tr>').get(0);
+  this.linha = $('<tr class="ultima" style="font-family: Arial; font-size: 8pt;"><td>' + DateHelper.toLocaleDate(data) + '</td>' + celulaVazia + '<td class="erro">Falta</td>' + celulaVazia.repeat(2) + '</tr>').get(0);
 }
 Falta.prototype = Object.create(Registro.prototype);
 Falta.prototype.constructor = Falta;
